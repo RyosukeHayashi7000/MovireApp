dd4f5f589f4a5fbf603e6fdd577008c2fe959bb5
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-04-02 10:51:34 +0900

    Retrofitインスタンスを作成し、非同期処理のメソッドを追加

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.movieapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.movieapp.model.MovieDBResponse;
+import com.example.movieapp.Service.MovieDataService;
+import com.example.movieapp.Service.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("TMBD Popular Movies Today");
+
+        getPopularMovies();
+    }
+    public void getPopularMovies(){
+        //インターフェースのMovieDBResponse型のRetrofitインスタンス（BASEURL指定済）を作成
+        MovieDataService movieDataService= RetrofitInstance.getService();
+
+        //インターフェースのMovieDBResponseのgetPopularMoviesメソッドからapi_keyを取得したRetrofitインスタンスを
+        //MovieDBResponseクラス型のCallクラスのインスタンスcallに入れる
+        Call<MovieDBResponse> call = movieDataService.getPopularMovies(this.getString(R.string.api_key));
+
+        call.enqueue(new Callback<MovieDBResponse>() {
+            @Override
+            public void onResponse(Call<MovieDBResponse> call, Response<MovieDBResponse> response) {
+
+                MovieDBResponse movieDBResponse = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<MovieDBResponse> call, Throwable t) {
+
+            }
+        });
+
     }
 }
