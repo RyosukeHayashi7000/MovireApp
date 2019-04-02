eff1fa5b7ec2cce583d59ad6c5d96afeacd2a78c
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-04-02 10:52:49 +0900

    apiを取得するgetメソッドを明記

--- app/src/main/java/com/example/movieapp/Service/MovieDataService.java
+++ app/src/main/java/com/example/movieapp/Service/MovieDataService.java
@@ -1,11 +1,13 @@
 package com.example.movieapp.Service;
 
+import com.example.movieapp.model.MovieDBResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface MovieDataService {
 
-    @GET("movie/popular")
-    Call<com.MovieDBResponse.movieapp.model.MovieDBResponse> getPpularMovies(@Query("api_key") String api_key);
+    @GET("movie/popular") //エンドポイント
+    Call<MovieDBResponse> getPopularMovies(@Query("api_key") String api_key);
 }
