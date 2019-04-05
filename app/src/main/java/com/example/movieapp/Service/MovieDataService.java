d2b1611c5a3edbe3590fa9142d624e6616a55761
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-04-05 17:26:10 +0900

    非同期処理をRxJavaに変更

--- app/src/main/java/com/example/movieapp/Service/MovieDataService.java
+++ app/src/main/java/com/example/movieapp/Service/MovieDataService.java
@@ -2,12 +2,17 @@ package com.example.movieapp.Service;
 
 import com.example.movieapp.model.MovieDBResponse;
 
-import retrofit2.Call;
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface MovieDataService {
 
+    //enqueueメソッドでの非同期処理の場合
+//    @GET("movie/popular") //エンドポイント
+//    Call<MovieDBResponse> getPopularMovies(@Query("api_key") String api_key);
+
+    //RxJavaでの非同期処理の場合
     @GET("movie/popular") //エンドポイント
-    Call<MovieDBResponse> getPopularMovies(@Query("api_key") String api_key);
+    Observable<MovieDBResponse> getPopularMoviesWithRx(@Query("api_key") String api_key);
 }
