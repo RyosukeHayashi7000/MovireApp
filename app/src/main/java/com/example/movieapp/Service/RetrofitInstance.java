ffc343bf76c610d251ed8ed23f5d74b4d97d09f7
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-04-05 17:26:53 +0900

    OkHttpClientを追記

--- app/src/main/java/com/example/movieapp/Service/RetrofitInstance.java
+++ app/src/main/java/com/example/movieapp/Service/RetrofitInstance.java
@@ -1,6 +1,10 @@
 package com.example.movieapp.Service;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
@@ -11,11 +15,20 @@ public class RetrofitInstance {
     //インターフェースのMovieDataServiceの形で結果を受け取るメソッド
     public static MovieDataService getService(){
 
+
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .build();
+
         if(retrofit==null){
 
             retrofit=new Retrofit
                     .Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
