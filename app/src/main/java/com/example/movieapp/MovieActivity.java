35722380c2d454059f4906b581e56aa6face2750
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-04-05 16:13:21 +0900

    intentを受け取りImageViewと各TextViewに表示するようコーディング

--- app/src/main/java/com/example/movieapp/MovieActivity.java
+++ app/src/main/java/com/example/movieapp/MovieActivity.java
@@ -1,13 +1,25 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.movieapp.model.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private Movie movie;
+
+    private ImageView movieImage;
+
+    private String image;
+
+    private TextView movieTitle, movieSynopsis,movieRating,movieReleaseDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +28,40 @@ public class MovieActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        movieImage = (ImageView) findViewById(R.id.ivMovieLarge);
+        movieTitle = (TextView) findViewById(R.id.tvMovieTitle);
+        movieSynopsis = (TextView) findViewById(R.id.tvPlotsynopsis);
+        movieRating = (TextView) findViewById(R.id.tvMovieRating);
+        movieReleaseDate = (TextView) findViewById(R.id.tvReleaseDate);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("movie")){
+
+            movie = getIntent().getParcelableExtra("movie");
+
+            Toast.makeText(getApplicationContext(),movie.getOriginalTitle(),Toast.LENGTH_LONG).show();
+
+            image=movie.getPosterPath();
+
+            String path = "https://image.tmdb.org/t/p/w500" + image;
+
+            Glide.with(this)
+                    .load(path)
+                    .placeholder(R.drawable.loading)
+                    .into(movieImage);
+
+            getSupportActionBar().setTitle(movie.getTitle());
+
+            movieTitle.setText(movie.getTitle());
+            movieSynopsis.setText(movie.getOverview());
+            movieRating.setText(Double.toString(movie.getVoteAverage()));
+            movieReleaseDate.setText(movie.getReleaseDate());
+
+        }
     }
 
 }
